192770001e857db709e97758addb286a15425059
Aminah <98732130+Aminahchesoh@users.noreply.github.com>
2023-01-23 08:20:03 +0700

    Update myLastCalc.py

--- myLastCalc.py
+++ myLastCalc.py
@@ -1,58 +1,53 @@
 """
 Name: total
-Discribtion: calculate the total of two number
+Distribution: calculate the total of two number
 Author: Aminah
 """
+
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
 while True:
     try:
         # using exception handling
-        number1 = int(input("Input  the number:"))
-        number2 = int(input("Input  the number:"))
-        b = number1, number2
-
+        number1 = int(input("Input  the first number:"))
+        number2 = int(input("Input  the second number:"))
         z = (input("select the operator (+,-,*,/): "))
-
-
-        def add(x, y):
-            return x + y
-
-
-        # This function subtracts two numbers
-        def subtract(x, y):
-            return x - y
-
-
-        # This function multiplies two numbers
-        def multiply(x, y):
-            return x * y
-
-
-        # This function divides two numbers
-        def divide(x, y):
-            return x / y
-
-
-        # calculate the total of 2 number
-        if z == '+':
-            print(number1, '+', number2, '=', add(number1, number2))
-        elif z == '-':
-            print(number1, '-', number2, '=', subtract(number1, number2))
-        elif z == '*':
-            print(number1, '*', number2, '=', multiply(number1, number2))
-        elif z == '/':
-            print(number1, '/', number2, '=', divide(number1, number2))
-
-        else:
-            print("error, please check your code")
-
     except ZeroDivisionError:
         print('Cannot divide by zero')
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        continue
 
-    except:
-        print("error, please check your code")
+    # calculate the total of 2 number
+    if z == '+':
+        print(number1, '+', number2, '=', add(number1, number2))
+    elif z == '-':
+        print(number1, '-', number2, '=', subtract(number1, number2))
+    elif z == '*':
+        print(number1, '*', number2, '=', multiply(number1, number2))
+    elif z == '/':
+        print(number1, '/', number2, '=', divide(number1, number2))
 
     # check if user wants another calculation
     # break the while loop if answer is no
     next_calculation = input("Let's do next calculation? (yes/no): ")
     if next_calculation == "no":
         break
+
+    else:
+        print("please choose the correct operator")
